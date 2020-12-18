8ecef80423f9b90b377d16e54e51f94894d05f78
Oğuz Ergül <erguloguz@hotmail.com>
2020-12-18 21:09:36 +0300

    Components refactored and configured

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <carousel >
+      <carousel-slide v-for="slide in slides" :key="slide" class="carousel-slider">
+        <img :src="slide" :alt="slide">
+      </carousel-slide>
+    </carousel>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Carousel from '../../vue-image-slider/src/Carousel.vue';
+import CarouselSlide from '../../vue-image-slider/src/CarouselSlide';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      slides: [
+        'https://picsum.photos/id/230/600/300',
+        'https://picsum.photos/id/231/600/300',
+        'https://picsum.photos/id/232/600/300',
+        'https://picsum.photos/id/233/600/300',
+        'https://picsum.photos/id/234/600/300',
+        'https://picsum.photos/id/235/600/300',
+        'https://picsum.photos/id/236/600/300',
+      ]
+    }
+  },
+
+    components : {
+        Carousel : Carousel,
+        CarouselSlide : CarouselSlide,
+    }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+    .app {
+        display:flex;
+        justify-content: center;
+    }
+    .carousel {
+      position:relative;
+      overflow: hidden;
+      width:800px;
+      height:500px;
+      z-index:10;
+    }
+    .btn {
+      padding:5px 10px;
+      background-color:rgba(0,0,0,0.5);
+      border:1px solid transparent;
+      margin:5px 10px;
+      color:#FFF;
+      height:50px;
+      width:50px;
+      position:absolute;
+      margin-top:-25px;
+      z-index:2;
+    }
+    .btn:hover {
+        cursor: pointer;
+    }
+    .btn:focus{
+        outline:none;
+    }
+    .btn-next {
+        top:50%;
+        right:0;
+    }
+    .btn-prev {
+        top:50%;
+        left:0;
+    }
+    .carousel-slider {
+        position:absolute;
+        top:0;
+        left:0;
+        bottom:0;
+        right:0;
+    }
+    .carousel-slider img {
+        width:100%;
+        height:100%;
+    }
+
 </style>
