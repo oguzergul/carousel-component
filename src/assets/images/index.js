c10332556d9860c68106314cabf1b240a3c31738
oguzergul <erguloguz@hotmail.com>
2021-09-18 20:45:24 +0300

    Add: Slider images has been added.

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+export const IMAGES = [
+    'https://picsum.photos/id/1018/800/500',
+    'https://picsum.photos/id/1043/800/500',
+    'https://picsum.photos/id/1057/800/500',
+    'https://picsum.photos/id/116/800/500',
+    'https://picsum.photos/id/505/800/500',
+    'https://picsum.photos/id/517/800/500',
+    'https://picsum.photos/id/509/800/500',
+]
